02bf309196f94f368e5152278d79691cb261cf5e
Bogdan Naydenov <bnaydenov@gmail.com>
2023-07-17 03:56:06 -0400

    format 88

--- arrays/88-merge-sorted-arrays.py
+++ arrays/88-merge-sorted-arrays.py
@@ -6,16 +6,16 @@ class Solution:
 
         i = m - 1
         j = n - 1
-        k = m + n -1
+        k = m + n - 1
 
-        while i >= 0 and j >= 0 :
+        while i >= 0 and j >= 0:
 
             if nums1[i] > nums2[j]:
-               nums1[k] = nums1[i]
-               i -= 1
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-               nums1[k] = nums2[j]
-               j -= 1
+                nums1[k] = nums2[j]
+                j -= 1
 
             k -= 1
 
