464279dc40e9a8427add9e1848da2b38240af4de
Bogdan Naydenov <bnaydenov@gmail.com>
2023-07-18 10:55:42 -0400

    add 26-remove-elemets-sorted-array

---
+++ arrays/26-remove-element-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        # count = 0
+        #
+        # for i in range(len(nums)):
+        #
+        #     if (i < len(nums) - 1) and (nums[i] == nums[i + 1]):
+        #         continue
+        #     nums[count] = nums[i]
+        #     count += 1
+        #
+        # return count
+
+        # Time O(n) and Space O(n)
+        count = 0
+
+        for num in nums:
+            if count < 1 or num > nums[count - 1]:
+                nums[count] = num
+                count += 1
+        return count
