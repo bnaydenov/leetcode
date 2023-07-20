dedb29229ca818dfb23bd79a3a834b5384a8b1e5
Bogdan Naydenov <bnaydenov@gmail.com>
2023-07-20 10:39:52 -0400

    update 88 merge sorted arrays - solution from https://youtu.be/P1Ic85RarKY

--- arrays/88-merge-sorted-arrays.py
+++ arrays/88-merge-sorted-arrays.py
@@ -25,16 +25,35 @@ class Solution:
         # if j >= 0:
         #     onums1[:k+1] = nums2[:j+1]
 
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
+        # i = m - 1
+        # j = n - 1
+        # k = m + n - 1
+        #
+        # while (j >= 0):
+        #     if i >= 0 and nums1[i] > nums2[j]:
+        #         nums1[k] = nums1[i]
+        #         i -= 1
+        #         k -= 1
+        #     else:
+        #         nums1[k] = nums2[j]
+        #         j -= 1
+        #         k -= 1
+        #
+        # Solution here
+        # https://youtu.be/P1Ic85RarKY
+        last = m + n - 1
 
-        while (j >= 0):
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-                k -= 1
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-                k -= 1
+                nums1[last] = nums2[n-1]
+                n -= 1
+
+            last -= 1
+
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
