d8f72e682958601cf7f2169215e00688c2b72c11
Bogdan Naydenov <bnaydenov@gmail.com>
2023-09-13 16:49:19 -0400

    169 add youtube link

--- arrays/169-majority-elements.py
+++ arrays/169-majority-elements.py
@@ -2,6 +2,8 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # https://walkccc.me/LeetCode/problems/0169/
 
+        # https://youtu.be/7pnhv842keE
+
         res = None
         count = 0
 
